5ba432d0db3bb1a68565a2e79d1e04a0c35a29ee
nicksieger <nicksieger@8ba958d5-0c1a-0410-94a6-a65dfc1b28a6>
2007-11-29 22:08:29 +0000

    Add missing MRI test lib so we can run test_pair
    git-svn-id: svn+ssh://rubyforge.org/var/svn/jruby-extras/trunk/jopenssl@820 8ba958d5-0c1a-0410-94a6-a65dfc1b28a6

--- test/test_openssl.rb
+++ test/test_openssl.rb
@@ -13,18 +13,18 @@ else
 end
 
 if runTests
-# won't work, since kill and pid is used.
-#  require 'test/openssl/test_ssl'
-  require 'test/openssl/test_asn1'
-  require 'test/openssl/test_cipher'
-  require 'test/openssl/test_digest'
-  require 'test/openssl/test_hmac'
-  require 'test/openssl/test_ns_spki'
-  require 'test/openssl/test_pkey_rsa'
-  require 'test/openssl/test_x509cert'
-  require 'test/openssl/test_x509crl'
-  require 'test/openssl/test_x509name'
-  require 'test/openssl/test_x509ext'
-  require 'test/openssl/test_x509req'
-  require 'test/openssl/test_x509store'
+  require 'openssl/test_asn1'
+  require 'openssl/test_cipher'
+  require 'openssl/test_digest'
+  require 'openssl/test_hmac'
+  require 'openssl/test_ns_spki'
+  require 'openssl/test_pair'
+  require 'openssl/test_pkey_rsa'
+#  require 'openssl/test_ssl' # won't work, since kill and pid is used.
+  require 'openssl/test_x509cert'
+  require 'openssl/test_x509crl'
+  require 'openssl/test_x509name'
+  require 'openssl/test_x509ext'
+  require 'openssl/test_x509req'
+  require 'openssl/test_x509store'
 end
