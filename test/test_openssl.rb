f6565c15598dcc3ac2025387107588ada977f85d
nicksieger <nicksieger@8ba958d5-0c1a-0410-94a6-a65dfc1b28a6>
2007-11-29 23:12:04 +0000

    Comment out test_pair, also isn't working at the moment
    git-svn-id: svn+ssh://rubyforge.org/var/svn/jruby-extras/trunk/jopenssl@821 8ba958d5-0c1a-0410-94a6-a65dfc1b28a6

--- test/test_openssl.rb
+++ test/test_openssl.rb
@@ -1,26 +1,25 @@
 
+run_tests = true
 if RUBY_PLATFORM =~ /java/
   require 'java'
-  runTests = false
+  run_tests = false
   begin
     java.lang.StringBuilder
-    runTests = true
+    run_tests = true
   rescue 
     nil
   end
-else
-  runTests = true
 end
 
-if runTests
+if run_tests
   require 'openssl/test_asn1'
   require 'openssl/test_cipher'
   require 'openssl/test_digest'
   require 'openssl/test_hmac'
   require 'openssl/test_ns_spki'
-  require 'openssl/test_pair'
+  # require 'openssl/test_pair'
   require 'openssl/test_pkey_rsa'
-#  require 'openssl/test_ssl' # won't work, since kill and pid is used.
+  # require 'openssl/test_ssl' # won't work, since kill and pid is used.
   require 'openssl/test_x509cert'
   require 'openssl/test_x509crl'
   require 'openssl/test_x509name'
