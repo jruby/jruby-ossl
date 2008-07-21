6c71c6b77e5e1d734f05b41dc2750cba59ea22d9
olabini <olabini@8ba958d5-0c1a-0410-94a6-a65dfc1b28a6>
2008-07-21 18:43:06 +0000

    Add param and comment out stuff that doesn't compile right now
    git-svn-id: svn+ssh://rubyforge.org/var/svn/jruby-extras/trunk/jopenssl@1070 8ba958d5-0c1a-0410-94a6-a65dfc1b28a6

--- src/java/org/jruby/ext/openssl/impl/Mime.java
+++ src/java/org/jruby/ext/openssl/impl/Mime.java
@@ -27,6 +27,7 @@
  ***** END LICENSE BLOCK *****/
 package org.jruby.ext.openssl.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -35,7 +36,130 @@ import java.util.List;
  */
 public interface Mime {
     Mime DEFAULT = new Mime() {
+            private final static int MIME_INVALID = 0;
+            private final static int MIME_START = 1;
+            private final static int MIME_TYPE = 2;
+            private final static int MIME_NAME = 3;
+            private final static int MIME_VALUE = 4;
+            private final static int MIME_QUOTE = 5;
+            private final static int MIME_COMMENT = 6;
+
+            private final static int MAX_SMLEN = 1024;
+
             public List<MimeHeader> parseHeaders(BIO bio) {
+                int state = 0;
+                byte[] linebuf = new byte[MAX_SMLEN];
+                int len = 0;
+                MimeHeader mhdr = null;
+
+                List<MimeHeader> headers = new ArrayList<MimeHeader>();
+
+//                 while((len = bio.gets(linebuf, MAX_SMLEN)) > 0) {
+//                     if(mhdr != null && Character.isSpaceChar((char)linebuf[0])) {
+//                         state = MIME_NAME;
+//                     } else {
+//                         state = MIME_START;
+//                     }
+
+//                 }
+
+
+// 	char *p, *q, c;
+// 	char *ntmp;
+// 	char linebuf[MAX_SMLEN];
+// 	MIME_HEADER *mhdr = NULL;
+// 	STACK_OF(MIME_HEADER) *headers;
+// 	int len, state, save_state = 0;
+
+// 	headers = sk_MIME_HEADER_new(mime_hdr_cmp);
+// 	while ((len = BIO_gets(bio, linebuf, MAX_SMLEN)) > 0) {
+//         /* If whitespace at line start then continuation line */
+//         if(mhdr && isspace((unsigned char)linebuf[0])) state = MIME_NAME;
+//         else state = MIME_START;
+//         ntmp = NULL;
+//         /* Go through all characters */
+//         for(p = linebuf, q = linebuf; (c = *p) && (c!='\r') && (c!='\n'); p++) {
+
+//             /* State machine to handle MIME headers
+//              * if this looks horrible that's because it *is*
+//              */
+
+//             switch(state) {
+// 			case MIME_START:
+//                 if(c == ':') {
+//                     state = MIME_TYPE;
+//                     *p = 0;
+//                     ntmp = strip_ends(q);
+//                     q = p + 1;
+//                 }
+//                 break;
+
+// 			case MIME_TYPE:
+//                 if(c == ';') {
+//                     mime_debug("Found End Value\n");
+//                     *p = 0;
+//                     mhdr = mime_hdr_new(ntmp, strip_ends(q));
+//                     sk_MIME_HEADER_push(headers, mhdr);
+//                     ntmp = NULL;
+//                     q = p + 1;
+//                     state = MIME_NAME;
+//                 } else if(c == '(') {
+//                     save_state = state;
+//                     state = MIME_COMMENT;
+//                 }
+//                 break;
+
+// 			case MIME_COMMENT:
+//                 if(c == ')') {
+//                     state = save_state;
+//                 }
+//                 break;
+
+// 			case MIME_NAME:
+//                 if(c == '=') {
+//                     state = MIME_VALUE;
+//                     *p = 0;
+//                     ntmp = strip_ends(q);
+//                     q = p + 1;
+//                 }
+//                 break ;
+
+// 			case MIME_VALUE:
+//                 if(c == ';') {
+//                     state = MIME_NAME;
+//                     *p = 0;
+//                     mime_hdr_addparam(mhdr, ntmp, strip_ends(q));
+//                     ntmp = NULL;
+//                     q = p + 1;
+//                 } else if (c == '"') {
+//                     mime_debug("Found Quote\n");
+//                     state = MIME_QUOTE;
+//                 } else if(c == '(') {
+//                     save_state = state;
+//                     state = MIME_COMMENT;
+//                 }
+//                 break;
+
+// 			case MIME_QUOTE:
+//                 if(c == '"') {
+//                     mime_debug("Found Match Quote\n");
+//                     state = MIME_VALUE;
+//                 }
+//                 break;
+//             }
+//         }
+
+//         if(state == MIME_TYPE) {
+//             mhdr = mime_hdr_new(ntmp, strip_ends(q));
+//             sk_MIME_HEADER_push(headers, mhdr);
+//         } else if(state == MIME_VALUE)
+//             mime_hdr_addparam(mhdr, ntmp, strip_ends(q));
+//         if(p == linebuf) break;	/* Blank line means end of headers */
+//     }
+
+//     return headers;
+
+
                 return null;
             }
 
