jruby/jruby-ossl

commit 4a8aff635167f736663713b64cd262052cbf9986

Fix X509 cert extension parsing, especially subjectAltName
subjectAltName was incorrectly parsed when initializing
a cert from pem or der.
Fix also fixes the parsing of other critical extensions.

This fixes all ssl test_post_connection_check

Signed-off-by: Brice Figureau <brice@daysofwonder.com>
Signed-off-by: Charles Oliver Nutter <headius@headius.com>

src/java/org/jruby/ext/openssl/X509Cert.java
@@ -182,13 +182,15 @@ public class X509Cert extends RubyObject {
             for(Iterator iter = crit.iterator();iter.hasNext();) {
                 String critOid = (String)iter.next();
                 byte[] value = cert.getExtensionValue(critOid);
-                IRubyObject rValue = ASN1.decode(ossl.getConstant("ASN1"),RubyString.newString(runtime, value)).callMethod(tc,"value");
+                IRubyObject rValue = ASN1.decode(ossl.getConstant("ASN1"),RubyString.newString(runtime, value)).callMet
+hod(tc,"value");
                 if(critOid.equals("2.5.29.17")) {
-                    add_extension(extFact.callMethod(tc,"create_ext", new IRubyObject[]{runtime.newString(critOid),runtime.newString(rValue.toString()),runtime.getTrue()}));
+                    X509Extensions.Extension ext = (X509Extensions.Extension)(((RubyClass)(((RubyModule)(getRuntime().getModule("OpenSSL").getConstant("X509"))).getConstant("Extension"))).callMethod(tc,"new", new IRubyObject[]{runtime.newString(critOid),rValue,runtime.getTrue()}));
+                    add_extension(ext);
                 } else {
                     byte[] dest = new byte[value.length - 4];
                     System.arraycopy(value, 4, dest, 0, value.length - 4);
-                    add_extension(extFact.callMethod(tc,"create_ext", new IRubyObject[]{runtime.newString(critOid),runtime.newString(new ByteList(dest, false)),runtime.getTrue()}));
+                    add_extension(extFact.callMethod(tc,"create_ext", new IRubyObject[]{runtime.newString(critOid),runtime.newString(new ByteList(dest, false)),runtime.getFalse()}));
                 }
             }
         }

commit 39bd429bc75e4aea5cdd0ab468595140deda6cec

Fix a typo introduced in 4a8aff635167f736663713b64cd262052cbf9986.

src/java/org/jruby/ext/openssl/X509Cert.java
@@ -182,8 +182,7 @@ public class X509Cert extends RubyObject {
             for(Iterator iter = crit.iterator();iter.hasNext();) {
                 String critOid = (String)iter.next();
                 byte[] value = cert.getExtensionValue(critOid);
-                IRubyObject rValue = ASN1.decode(ossl.getConstant("ASN1"),RubyString.newString(runtime, value)).callMet
-hod(tc,"value");
+                IRubyObject rValue = ASN1.decode(ossl.getConstant("ASN1"),RubyString.newString(runtime, value)).callMethod(tc,"value");
                 if(critOid.equals("2.5.29.17")) {
                     X509Extensions.Extension ext = (X509Extensions.Extension)(((RubyClass)(((RubyModule)(getRuntime().getModule("OpenSSL").getConstant("X509"))).getConstant("Extension"))).callMethod(tc,"new", new IRubyObject[]{runtime.newString(critOid),rValue,runtime.getTrue()}));
                     add_extension(ext);