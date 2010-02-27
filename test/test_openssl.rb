74ee9e9ceba4d6fdabd647b0ce3750512599a475
Hiroshi Nakamura <nahi@ruby-lang.org>
2010-02-27 09:33:12 +0900

    let test_openssl.rb load openssl/test_* only.
    Use test_all.rb or rake test to run all testcases.

--- test/test_openssl.rb
+++ test/test_openssl.rb
@@ -1,43 +1,4 @@
-
-if defined?(JRUBY_VERSION)
-  require "java"
-  base = File.join(File.dirname(__FILE__), '..')
-  $CLASSPATH << File.join(base, 'pkg', 'classes')
-  $CLASSPATH << File.join(base, 'lib', 'bcprov-jdk15-144.jar')
+files = File.join(File.dirname(__FILE__), 'openssl', 'test_*.rb')
+Dir.glob(files).sort.each do |tc|
+  require tc
 end
-
-def protect_require(name)
-  require name
-rescue Exception => e
-  $stderr.puts "Had exception in #{name}: #{e.inspect}"
-  $stderr.puts(*(e.backtrace))
-end
-
-protect_require 'openssl/test_asn1'
-protect_require 'openssl/test_cipher'
-protect_require 'openssl/test_digest'
-protect_require 'openssl/test_hmac'
-protect_require 'openssl/test_ns_spki'
-protect_require 'openssl/test_pair'
-protect_require 'openssl/test_pkcs7'
-protect_require 'openssl/test_pkey_rsa'
-protect_require 'openssl/test_ssl'
-protect_require 'openssl/test_x509cert'
-protect_require 'openssl/test_x509crl'
-protect_require 'openssl/test_x509ext'
-protect_require 'openssl/test_x509name'
-protect_require 'openssl/test_x509req'
-protect_require 'openssl/test_x509store'
-protect_require 'test_certificate'
-protect_require 'test_cipher'
-protect_require 'test_integration'
-protect_require 'test_java'
-protect_require 'test_java_attribute'
-protect_require 'test_java_bio'
-protect_require 'test_java_mime'
-protect_require 'test_java_pkcs7'
-protect_require 'test_java_smime'
-protect_require 'test_openssl_x509'
-protect_require 'test_parse_certificate'
-protect_require 'test_pkey'
-protect_require 'test_x509store'
